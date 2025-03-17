6066644c2245bc72f1d1e1d7269a5c9a05f432ba
Adel Shurrab <143008607+Adel-Shurrab@users.noreply.github.com>
2025-03-17 17:33:10 +0200

    Update plot_maze.py

--- code/visualization/plot_maze.py
+++ code/visualization/plot_maze.py
@@ -2,7 +2,14 @@
 from pyamaze import maze, agent, textLabel
 
 def plot_maze(m, path, algorithm_name):
-    """Visualize the maze and the solution path."""
+    """
+    Visualize the maze and the solution path.
+
+    Parameters:
+    m (maze): The maze object.
+    path (dict): The solution path as a dictionary of cell connections.
+    algorithm_name (str): The name of the algorithm used to solve the maze.
+    """
     a = agent(m, footprints=True, filled=True)
     m.tracePath({a: path})
     textLabel(m, f'{algorithm_name} Path Length', len(path) + 1)
